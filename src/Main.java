a3019a6e8dc873e1981d302496c367ef1f1afdd0
CautiousPear <you@example.com>
2025-03-01 12:47:49 -0600

    Fixed turn order error & tie scenario

--- src/Main.java
+++ src/Main.java
@@ -11,39 +11,28 @@ public class Main {
         while(true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Hit or stand?");
-            if (!scanner.next().equalsIgnoreCase("hit")) {
-                while(dealerRand < 17 || (dealerRand <= yourRand)) {
-                    dealerRand = dealerRand + rand.nextInt(8) + 2;
+            if (!scanner.next().contains("h")) { // If user stands
+                while(dealerRand < 17 || (dealerRand < yourRand)) {
+                    dealerRand = dealerRand + rand.nextInt(9) + 1;
                     System.out.println("Dealer hit and has " + dealerRand + " points.");
-                    if(dealerRand > 21) {
-                        System.out.println("Bust!");
-                        System.out.println("You win with " + yourRand + " points!");
-                        break;
-                    }
                 }
-                if (yourRand > dealerRand) {
-                    System.out.println("You win with "+ yourRand + " points! Dealer had " + dealerRand + " points.");
+                if(dealerRand > 21) {
+                    System.out.println("Bust!");
+                    System.out.println("You win with " + yourRand + " points!");
                 } else if (dealerRand > yourRand) {
                     System.out.println("You lose with "+ yourRand + " points! Dealer had " + dealerRand + " points.");
-                } else
-                    System.out.println("You tied the dealer! Both had " + yourRand +" points.");
+                } else {
+                    System.out.println("You tied the dealer! Both had " + yourRand + " points.");
+                }
                 break;
-            } else {
-                yourRand = yourRand + rand.nextInt(8) + 2;
+            } else { //If user hits
+                yourRand = yourRand + rand.nextInt(9) + 1;
                 System.out.println("Your hand = " + yourRand);
+                System.out.println("Dealer hand = "+ dealerRand);
                 if(yourRand > 21) {
                     System.out.println("Bust!");
                     System.out.println("You lose with "+ yourRand + " points!");
                     break;
-                }
-                if (dealerRand < 17 && (dealerRand <= yourRand)) {
-                    dealerRand = dealerRand + rand.nextInt(8) + 2;
-                    System.out.println("Dealer hand = " + dealerRand);
-                    if(dealerRand > 21) {
-                        System.out.println("Bust!");
-                        System.out.println("You win with " + yourRand + " points!");
-                        break;
-                    }
                 }
             }
 
